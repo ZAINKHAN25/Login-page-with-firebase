636635dda92402ca0db40172adde4d998240145b
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-05-19 20:34:02 +0500

    authentication lga di

--- script.js
+++ script.js
@@ -23,8 +23,19 @@ function submitForm(e) {
   var name = getElementVal("name");
   var emailid = getElementVal("emailid");
   var msgContent = getElementVal("msgContent");
+  
+  
+if(name == ""|| emailid == ""||msgContent ==""){
+  //   enable alert
+  document.querySelector(".alert2").style.display = "block";
 
-  saveMessages(name, emailid, msgContent);
+  //   remove the alert
+  setTimeout(() => {
+    document.querySelector(".alert2").style.display = "none";
+  }, 3000);
+}
+
+else{
 
   //   enable alert
   document.querySelector(".alert").style.display = "block";
@@ -36,17 +47,21 @@ function submitForm(e) {
 
   //   reset the form
   document.getElementById("contactForm").reset();
+  const saveMessages = (name, emailid, msgContent) => {
+    var newContactForm = contactFormDB.push();
+    newContactForm.set({
+      name: name,
+      emailid: emailid,
+      msgContent: msgContent,
+    });
+  
 }
+    saveMessages(name, emailid, msgContent);
 
-const saveMessages = (name, emailid, msgContent) => {
-  var newContactForm = contactFormDB.push();
 
-  newContactForm.set({
-    name: name,
-    emailid: emailid,
-    msgContent: msgContent,
-  });
 };
+
+}
 
 const getElementVal = (id) => {
   return document.getElementById(id).value;
